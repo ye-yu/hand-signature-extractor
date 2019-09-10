99b37919db6307d08f0b2aa103ef6af07baabbb7
unknown <rafolwen98@gmail.com>
2019-09-11 00:43:43 +0800

    Reset commits

--- extract_signature.py
+++ extract_signature.py
@@ -75,10 +75,9 @@ def obtain_signature(image_source,
     imshow_verbose(image, cmap='gray', image_size=(inline if type(inline) == int else 10))
     if threshold:
         pr('Binarising with threshold at', threshold)
-        imgf = image
-        imgf[imgf >= threshold] = 255
-        imgf[imgf < threshold] = 0
-        imagem = cv2.bitwise_not(imgf)
+        imagem = image
+        imagem[imagem >= threshold] = 255
+        imagem[imagem < threshold] = 0
 
     else:
         ret, imgf = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
